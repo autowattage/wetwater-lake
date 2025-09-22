6f175e98aa253c20a9ed49d893823487e25d0634
bunnyguy <pipandw@gmail.com>
2025-09-22 16:46:23 -0500

    got the depth meter working!

--- scenes/depth_bar.gd
+++ scenes/depth_bar.gd
@@ -1,8 +1,14 @@
 extends TextureProgressBar
 
-#@onready var HEIDI = get_node("/main/heidi")
-#func _process(_delta: float) -> void:
-	#print(HEIDI.position.y)
+@onready var heidi = get_node("../../../heidi")
+var top_clamp
+var bottom_clamp
+func _ready() -> void:
+	top_clamp = heidi.position.y
+	bottom_clamp = 1536
+
 	
-#func _on_value_changed(value: float) -> void:
-	#heidi_lv.position.y = value*texture_under.get_height()
+func _process(_delta: float) -> void:
+	value = clamp(heidi.position.y/(bottom_clamp-top_clamp)-.2,0,1)
+func _on_value_changed(value: float) -> void:
+	$"heidi-lv".position.y = -5+value*271
