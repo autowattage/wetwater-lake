acca359123bdf07ba664c01071d6be862a0c1419
bunnyguy <pipandw@gmail.com>
2025-09-24 10:51:56 -0500

    global water signal/variables, depth tank functionality YAY!

---
+++ scripts/hud.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+@onready var watertank_bar: TextureProgressBar = $"status-bars/watertank-bar"
+@onready var depth_bar: TextureProgressBar = $"status-bars/depth-bar"
+@onready var heidi = get_node("../heidi")
+
+var top_pos_clamp
+var bottom_pos_clamp
+
+func _ready() -> void:
+	top_pos_clamp = heidi.position.y
+	bottom_pos_clamp = 1536
+
+func _process(delta: float) -> void:
+	depth_bar.value = heidi.position.y/(bottom_pos_clamp-top_pos_clamp)-.2
+	if WaterDetect.inwater:
+		watertank_bar.value -= 0.025 * delta
+	else:
+		watertank_bar.value += 0.025 * delta
+
+func _on_depthbar_value_changed(value: float) -> void:
+	depth_bar.get_node("heidi-lv").position.y = -5+depth_bar.value*271
+
+func _on_watertankbar_value_changed(value: float) -> void:
+	watertank_bar.get_node("arrow").position.y = 269-watertank_bar.value*271
--- scripts/water_detect.gd
+++ scripts/water_detect.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 signal entered_water
 signal exited_water
+var inwater = 0
 
-func _on_area_entered(area: Area2D) -> void:
+func _on_area_entered(_area: Area2D) -> void:
+	WaterDetect.inwater = 0
 	exited_water.emit()
 	
-func _on_area_exited(area: Area2D) -> void:
+func _on_area_exited(_area: Area2D) -> void:
+	WaterDetect.inwater = 1
 	entered_water.emit()
